d12575f00d0481692bec30d091d9598698771bae
Macarena Aviles <esperanzamacarenaaviles@gmail.com>
2022-08-05 15:44:42 +0200

    Refactor Plural class

--- app/src/main/java/plural/Plural.java
+++ app/src/main/java/plural/Plural.java
@@ -3,12 +3,10 @@ package plural;
 public class Plural {
 
     public boolean checkNumber(Object input) {
-        boolean result;
         if (input.equals(1)) {
-            result = false;
-            return result;
+            return false;
         }
 
-        return result = true;
+        return true;
     }
 }
