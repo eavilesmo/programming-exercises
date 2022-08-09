92abe2009448722e12c43ff37d2d0c86079d8f23
Macarena Aviles <esperanzamacarenaaviles@gmail.com>
2022-08-09 21:35:26 +0200

    Test receive_a_list_and_return_a_number_composed_of_the_lists_elements_test passed

--- app/src/main/java/sumTwoArrays/SumTwoArrays.java
+++ app/src/main/java/sumTwoArrays/SumTwoArrays.java
@@ -3,12 +3,13 @@ package sumTwoArrays;
 import java.util.ArrayList;
 
 public class SumTwoArrays {
-    public String addArrays(ArrayList<Integer> listOfNumbers) {
+    public int addArrays(ArrayList<Integer> listOfNumbers) {
         String stringResult = "";
         for (int number : listOfNumbers) {
             String convertedNumber = Integer.toString(number);
             stringResult += convertedNumber;
         }
-        return stringResult;
+        int integerResult = Integer.parseInt(stringResult);
+        return integerResult;
     }
 }
--- app/src/test/java/sumTwoArrays/SumTwoArraysTests.java
+++ app/src/test/java/sumTwoArrays/SumTwoArraysTests.java
@@ -6,16 +6,29 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class SumTwoArraysTests {
 
+//    @Test
+//    void receive_a_list_and_return_a_string_composed_of_the_lists_elements_test() {
+//        SumTwoArrays sumTwoArrays = new SumTwoArrays();
+//        ArrayList<Integer> listOfNumbers = new ArrayList<>();
+//        listOfNumbers.add(2);
+//        listOfNumbers.add(7);
+//        listOfNumbers.add(1);
+//        listOfNumbers.add(5);
+//        String actualResult = sumTwoArrays.addArrays(listOfNumbers);
+//        String expectedResult = "2715";
+//        assertThat(actualResult).isEqualTo(expectedResult);
+//    }
+
     @Test
-    void receive_a_list_and_return_a_string_composed_of_the_lists_elements_test() {
+    void receive_a_list_and_return_a_number_composed_of_the_lists_elements_test() {
         SumTwoArrays sumTwoArrays = new SumTwoArrays();
         ArrayList<Integer> listOfNumbers = new ArrayList<>();
         listOfNumbers.add(2);
         listOfNumbers.add(7);
         listOfNumbers.add(1);
         listOfNumbers.add(5);
-        String actualResult = sumTwoArrays.addArrays(listOfNumbers);
-        String expectedResult = "2715";
+        int actualResult = sumTwoArrays.addArrays(listOfNumbers);
+        int expectedResult = 2715;
         assertThat(actualResult).isEqualTo(expectedResult);
     }
 }
